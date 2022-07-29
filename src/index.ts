0e4aa4251c6dd2f37dd62e93a8a59f94ea7ee5be
Sherif Hesham <sherifeldoksh8@gmail.com>
2022-07-29 21:50:20 +0200

    add types and return types to functions and callback

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,13 @@ import express from 'express';
 import routes from './routes/index';
 
 const app = express();
-const port = 3000;
+const port : number = 3000;
 
-app.listen(port, () => {
+app.listen(port, () : void => {
     console.log(`server running at http://localhost:${port}`);
 });
 
-app.get('/', (req, res) => {
+app.get('/', (req: express.Request, res: express.Response) : void => {
     res.send('Server is running');
 });
 
